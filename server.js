8e828ecd099a5d683f78273086b7be3e37082538
Dan Salmon <daniel.salmon@southcentral.edu>
2020-04-06 15:35:14 -0700

    Change port to 5000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // Set up a view engine to hbs
 app.set('view engine', 'hbs');
